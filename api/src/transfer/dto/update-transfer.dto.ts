de7b3c5055e31d11466c4598b5a5313cdd877561
ruben-gutierrez <rubengutierrez@unicauca.edu.co>
2023-07-17 19:18:27 -0500

    dto update

--- api/src/transfer/dto/update-transfer.dto.ts
+++ api/src/transfer/dto/update-transfer.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTransferDto } from './create-transfer.dto';
 
-export class UpdateTransferDto extends PartialType(CreateTransferDto) {}
+import { IsNotEmpty } from 'class-validator';
+
+export class UpdateTransferDto extends PartialType(CreateTransferDto) {
+    @IsNotEmpty()
+    id: number;
+}
