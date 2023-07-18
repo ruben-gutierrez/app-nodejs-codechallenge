935e6e63160bf1a96f8933f4a1f460cd60f25f8b
ruben-gutierrez <rubengutierrez@unicauca.edu.co>
2023-07-17 19:16:32 -0500

    add dto

--- api/src/transfer/dto/create-transfer.dto.ts
+++ api/src/transfer/dto/create-transfer.dto.ts
@@ -1,1 +1,29 @@
-export class CreateTransferDto {}
+import { IsNotEmpty, IsNumber, IsString, MaxLength, Min, Max } from 'class-validator';
+
+export class CreateTransferDto {
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
+    accountExternalIdDebit: string;
+  
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
+    accountExternalIdCredit: string;
+
+    @IsNumber()
+    @Min(1)
+    @Max(10)
+    tranferTypeId: number;
+
+    @IsNumber()
+    @Min(1)
+    @Max(10)
+    statusId: number;
+
+    @IsNumber()
+    @Min(1)
+    @Max(1000)
+    value: number;
+  
+}
